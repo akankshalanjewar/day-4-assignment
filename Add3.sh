6ad372237876e9cd951b5c6ecd6c309513a5e09f
Akanksha <akankshalanjewar02@gmail.com>
2022-02-24 20:22:44 +0530

    Added:example

---
+++ Add3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/ -x
+
+     read -p "Enter first number:" a
+     read -p "Enter second number:" b
+     read -p "Enter three number:" c
+     read -p "Enter fourth number:" d
+     read -p "Enter fifth number:" e 
+     z=$(($a + $b + $c + $d + $e))
+echo $z
+     avg=$(($z / 5)) 
+    
+
